47efa424f5ac191f7f5c4c03889780b1e3a5f6ab
artFetisov <fetishfestoff@mail.ru>
2023-12-31 01:49:35 +0300

    a lot of changes

---
+++ src/store/index.ts
@@ -0,0 +1,16 @@
+import { reducers } from './reducers/rootReducer'
+import { configureStore } from '@reduxjs/toolkit'
+
+export const store = configureStore({
+	reducer: reducers,
+	devTools: process.env.NODE_ENV !== 'production',
+	middleware: getDefaultMiddleware =>
+		getDefaultMiddleware({
+			serializableCheck: false,
+		}),
+})
+
+export type AppDispatch = typeof store.dispatch
+export type AppRootState = ReturnType<typeof store.getState>
+export type AppStore = ReturnType<typeof store>
+
