61322460cf609e1adadfe11a6a36700d31a9be54
artFetisov <fetishfestoff@mail.ru>
2024-05-30 00:07:49 +0300

    ref

--- src/components/layout/Header/Links/OrdersLink/OrdersLink.tsx
+++ src/components/layout/Header/Links/OrdersLink/OrdersLink.tsx
@@ -12,53 +12,16 @@ interface IOrdersLinkProps {
 }
 
 export const OrdersLink: FC<IOrdersLinkProps> = ({ isAuth, countItems }) => {
-	const {
-		handleMouseLeavePopup,
-		isHoveredElement,
-		isHoveredPopup,
-		handleMouseEnterPopup,
-		handleMouseEnterElement,
-		handleMouseLeaveElement,
-		coordinates,
-		targetRef,
-	} = usePopup<HTMLAnchorElement>()
-
-	useEffect(() => {
-		return () => console.log('destroy')
-	}, [])
-
 	return (
-		<>
-			{(isHoveredElement || isHoveredPopup) &&
-				createPortal(
-					<Popup
-						size={{ width: 300, max: 412 }}
-						position='bottom-center'
-						isShow={isHoveredElement}
-						onMouseEnter={handleMouseEnterPopup}
-						onMouseLeave={handleMouseLeavePopup}
-						coordinates={coordinates}
-					>
-						<div>popup</div>
-					</Popup>,
-					document.body
-				)}
-			<Link
-				ref={targetRef}
-				className={styles.link}
-				href={PATHS.MY_ORDER_LIST}
-				onMouseEnter={handleMouseEnterElement}
-				onMouseLeave={handleMouseLeaveElement}
-			>
-				{isAuth && <span className={styles.control}>{countItems}</span>}
-				<svg width={24} height={24}>
-					<path
-						fill='currentColor'
-						d='M12.486 1.126a1 1 0 0 0-.972 0l-9 5A1 1 0 0 0 2 7v9.98a1.003 1.003 0 0 0 .515.894l9 5a1 1 0 0 0 .989-.01l8.982-4.99A1 1 0 0 0 22 17V7a1 1 0 0 0-.514-.874l-9-5ZM11 20.3l-7-3.888V8.7l2 1.11V13a1 1 0 1 0 2 0v-2.078l3 1.666v7.713ZM7.5 8.356 5.06 7 12 3.144 14.441 4.5l-6.94 3.856ZM9.56 9.5l6.94-3.856L18.941 7l-6.94 3.856L9.558 9.5ZM13 20.3v-7.712L20 8.7v7.712L13 20.3Z'
-					></path>
-				</svg>
-				<span>{'Заказы'}</span>
-			</Link>
-		</>
+		<Link className={styles.link} href={PATHS.MY_ORDER_LIST}>
+			{isAuth && <span className={styles.control}>{countItems}</span>}
+			<svg width={24} height={24}>
+				<path
+					fill='currentColor'
+					d='M12.486 1.126a1 1 0 0 0-.972 0l-9 5A1 1 0 0 0 2 7v9.98a1.003 1.003 0 0 0 .515.894l9 5a1 1 0 0 0 .989-.01l8.982-4.99A1 1 0 0 0 22 17V7a1 1 0 0 0-.514-.874l-9-5ZM11 20.3l-7-3.888V8.7l2 1.11V13a1 1 0 1 0 2 0v-2.078l3 1.666v7.713ZM7.5 8.356 5.06 7 12 3.144 14.441 4.5l-6.94 3.856ZM9.56 9.5l6.94-3.856L18.941 7l-6.94 3.856L9.558 9.5ZM13 20.3v-7.712L20 8.7v7.712L13 20.3Z'
+				></path>
+			</svg>
+			<span>{'Заказы'}</span>
+		</Link>
 	)
 }
--- src/components/screens/home/Home.tsx
+++ src/components/screens/home/Home.tsx
@@ -7,6 +7,9 @@ import styles from './Home.module.scss'
 import { useActions } from '@/hooks/useActions'
 import { categoriesMock, subCategoriesMock, subSubCategoriesMock } from '@/mock/categories'
 import { currentAuthUser } from '@/mock/user'
+import Image from 'next/image'
+import bannerComp from '@/assets/images/2832_200_main_static_desk_11zon_17.webp'
+import bannerCompPub from '@/../public/2832_200_main_static_desk_11zon_17.webp'
 
 export const Home: FC = () => {
 	const { setCategories, setSubCategories, setSubSubCategories, setUserData } = useActions()
@@ -20,6 +23,21 @@ export const Home: FC = () => {
 
 	return (
 		<div className={styles.home}>
+			<div className={styles.banner}>
+				<div className={styles.imgBox}>
+					<Image alt='banner' src={'/2832_200_main_static_desk_11zon_17.webp'} fill />
+				</div>
+			</div>
+			<div className={styles.banner}>
+				<div className={styles.imgBox}>
+					<Image alt='banner' src={bannerComp} fill />
+				</div>
+			</div>
+			<div className={styles.banner}>
+				<div className={styles.imgBox}>
+					<Image alt='banner' src={bannerCompPub} fill />
+				</div>
+			</div>
 			<Selection items={productsMock.slice(0, 12)} rowTotalItems={6} headingText={'Лучшие предложения'} />
 			<Selection items={productsMock.slice(0, 10)} rowTotalItems={5} headingText={'Вы искали'} />
 			<Selection items={productsMock.slice(0, 6)} rowTotalItems={6} headingText={'Рекомендуем для вас'} />
